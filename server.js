53eb0a5cea1727a3fd0ce866e1c649f48f48099b
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-08-23 13:35:16 +0300

    ServerControler

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
+const session = require('express-session');
+const MongoStore = require('connect-mongo').MongoStore;
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+
+const authCtrl = require('./controllers/authCtrl');
+const booksCtrl = require('./controllers/booksCtrl.js');
+const usersCtrl = require('./controllers/usersCtrl.js')
+
+
+const port = process.env.PORT ? process.env.PORT : '3003';
+
 
 // Database Connection
 const mongoose = require('mongoose');
