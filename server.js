013b4c791f2fc3a71b8eb847a375d55d613b8974
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-08-25 21:04:59 +0300

    fixCloudinary

--- server.js
+++ server.js
@@ -1,39 +1,25 @@
 require('dotenv').config();
 require('./config/database');
 
-
 const path = require('path');
 const express = require('express');
 const app = express();
 
-// Middleware
 const session = require('express-session');
 const MongoStore = require('connect-mongo').MongoStore;
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
-const upload = require("./config/multer");
-
+const upload = require('./config/multer.js');
 
 const authCtrl = require('./controllers/authCtrl');
 const booksCtrl = require('./controllers/booksCtrl');
-const usersCtrl = require('./controllers/usersCtrl');
-//const isSignedIn = require('./middleware/is-signed-in');
 
-const port = process.env.PORT ? process.env.PORT : '3003';
+const port = process.env.PORT || 3003;
 
-
-
-
-
-
-
-// Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
-// Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride('_method'));
-// Morgan for logging HTTP requests
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -50,12 +36,9 @@ app.use(passUserToView);
 
 // PUBLIC ROUTES
 app.get('/', (req, res) => {
-    // Check if the user is signed in
     if (req.session.user) {
-        // Redirect signed-in users to their applications index
         res.redirect(`/users/${req.session.user._id}/books`);
     } else {
-        // Show the homepage for users who are not signed in
         res.render('index.ejs');
     }
 });
@@ -65,22 +48,22 @@ app.post('/auth/sign-up', authCtrl.register);
 app.get('/auth/sign-in', authCtrl.signin);
 app.post('/auth/sign-in', authCtrl.login);
 
-// Customer middleware
 app.use(isSignedIn);
 
 // PRIVATE ROUTES
 app.get('/auth/sign-out', authCtrl.signout);
 
-// Applications
 app.get('/users/:userId/books', booksCtrl.index);
-app.post("/books", isSignedIn, upload.single("image"), booksCtrl.create);
 app.get('/users/:userId/books/new', booksCtrl.new);
-app.post('/users/:userId/books', booksCtrl.create);
+app.post('/users/:userId/books', upload.single('image'), booksCtrl.create);
 app.get('/users/:userId/books/:bookId', booksCtrl.show);
-// app.put("/books/:Id", isSignedIn, upload.single("image"), booksCtrl.editBook)
-app.delete('/users/:userId/books/:bookId', booksCtrl.delete);
 app.get('/users/:userId/books/:bookId/edit', booksCtrl.edit);
-app.put('/users/:userId/books/:bookId', booksCtrl.update);
+app.put('/users/:userId/books/:bookId', upload.single('image'), booksCtrl.update);
+app.delete('/users/:userId/books/:bookId', booksCtrl.delete);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 // Users
 /*app.get('/users', usersCtrl.index);
@@ -89,11 +72,3 @@ app.listen(port, () => {
     console.log(`The express app is ready on port ${port}!`);
 });
 */
-
-
-
-
-
-app.listen(3003, () => {
-  console.log('Server is running on http://localhost:3003');
-});
