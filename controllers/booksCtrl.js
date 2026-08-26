b22924ce9a98b3440074db73133e717334946df8
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-08-26 10:28:32 +0300

    Revert "fixCloudinary"
    This reverts commit 013b4c791f2fc3a71b8eb847a375d55d613b8974.

--- controllers/booksCtrl.js
+++ controllers/booksCtrl.js
@@ -1,216 +1,140 @@
+
+const express = require('express');
+
 const User = require('../models/user.js');
-const Book = require('../models/book.js');
-const Writer = require('../models/writer');
-const cloudinary = require('../config/cloudinary.js');
+const Book = require('../models/book.js')
 
 const uploadImage = (fileBuffer) => {
     return new Promise((resolve, reject) => {
         const uploadStream = cloudinary.uploader.upload_stream(
             {
-                resource_type: 'image',
-                folder: 'book-store',
+                resource_type: 'image'
             },
             (error, result) => {
                 if (error) {
-                    reject(error);
+                    reject(error)
                 } else {
-                    resolve(result);
+                    resolve(result)
                 }
             }
-        );
+        )
 
-        uploadStream.end(fileBuffer);
-    });
-};
+        uploadStream.end(fileBuffer)
+    })
+}
 
-const newBook = async (req, res) => {
-  try {
-    const writers = await Writer.find({});
-    res.render('books/new.ejs', { 
-      title: 'Add New Book', 
-      writers: writers 
-    });
-  } catch (err) {
-    console.error(err);
-    res.redirect('/books');
-  }
-};
-const index = async (req, res) => {
+
+
+const newBook = async(req, res) => {
     try {
-        const user = await User.findById(req.params.userId).populate('books');
+        res.render('books/new.ejs');
+    } catch (err) {
+        res.redirect('/');
+    }
+};
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
 
-        res.render('books/index.ejs', {
-            user,
-            books: user.books,
-        });
+
+const show = async(req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const book = user.books.findById(req.params.bookId);
+             const img=user.books.img(req.params.bookImg)
+        res.render('books/show.ejs', { book });
     } catch (err) {
         console.log(err);
         res.redirect('/');
     }
 };
 
-const show = async (req, res) => {
+
+
+const create = async(req, res) => {
     try {
         const user = await User.findById(req.params.userId);
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
+        const uploadedImage = await uploadImage(req.file.buffer);
 
-        const ownsBook = user.books.some(
-            (bookId) => bookId.toString() === req.params.bookId
-        );
-
-        if (!ownsBook) {
-            return res.status(404).send('Book not found');
-        }
-
-        const book = await Book.findById(req.params.bookId);
-
-        if (!book) {
-            return res.status(404).send('Book not found');
-        }
-
-        res.render('books/show.ejs', { user, book });
-    } catch (err) {
-        console.log(err);
-        res.redirect('/');
-    }
-};
-const create = async (req, res) => {
-  try {
     const bookData = {
-      title: req.body.title,
-      description: req.body.description,
-      writer: req.body.writer,
-      image: req.file ? req.file.path : 'https://placeholder.com'
-    };
+        name: req.body.name,
+        price: req.body.price,
+        writer: req.body.writer,
+        description: req.body.description,
+        image: {
+            url: uploadedImage.secure_url,
+            publicId: uploadedImage.public_id
+        }
+    }
 
-    const newBook = await Book.create(bookData);
+    if (req.body.image) {
+        bookData.image = req.body.image;
+    }
+        await Book.create(bookData)
 
-    await Writer.findByIdAndUpdate(req.body.writer, {
-      $push: { books: newBook._id }
-    });
 
-    res.redirect('/books');
-  } catch (err) {
-    console.error(err);
-    res.redirect('/books/new');
-  }
+        res.redirect(`/users/${req.params.userId}/books`);
+    } catch (err) {
+        console.log(err);
+        res.redirect('/users/:userId/books/new');
+    }
 };
-const edit = async (req, res) => {
+
+
+
+const deleteBook = async(req, res) => {
     try {
         const user = await User.findById(req.params.userId);
+        user.books.pull(req.params.bookId);
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
+        await user.save();
 
-        const ownsBook = user.books.some(
-            (bookId) => bookId.toString() === req.params.bookId
-        );
-
-        if (!ownsBook) {
-            return res.status(404).send('Book not found');
-        }
-
-        const book = await Book.findById(req.params.bookId);
-
-        if (!book) {
-            return res.status(404).send('Book not found');
-        }
-
-        res.render('books/edit.ejs', { user, book });
+        res.redirect(`/users/${user._id}/books`);
     } catch (err) {
         console.log(err);
         res.redirect('/');
     }
 };
 
-const update = async (req, res) => {
+const edit = async(req, res) => {
     try {
         const user = await User.findById(req.params.userId);
+        const book = user.books.findById(req.params.bookId);
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
+        res.render('books/edit.ejs', { book });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    }
+};
 
-        const ownsBook = user.books.some(
-            (bookId) => bookId.toString() === req.params.bookId
-        );
 
-        if (!ownsBook) {
-            return res.status(404).send('Book not found');
-        }
 
-        const book = await Book.findById(req.params.bookId);
+const update = async(req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const book = user.books.findById(req.params.bookId);
 
-        if (!book) {
-            return res.status(404).send('Book not found');
-        }
+        book.set(req.body);
 
-        book.name = req.body.name;
-        book.price = req.body.price;
-        book.writer = req.body.writer;
-        book.description = req.body.description;
-
-        if (req.file) {
-            const oldPublicId = book.image.publicId;
-            const uploadedImage = await uploadImage(req.file.buffer);
-
-            book.image = {
-                url: uploadedImage.secure_url,
-                publicId: uploadedImage.public_id,
-            };
-
-            await book.save();
-
-            if (oldPublicId) {
-                await cloudinary.uploader.destroy(oldPublicId);
-            }
-        } else {
-            await book.save();
-        }
+        await user.save();
 
         res.redirect(`/users/${user._id}/books/${book._id}`);
     } catch (err) {
         console.log(err);
-        res.redirect(`/users/${req.params.userId}/books/${req.params.bookId}/edit`);
+        res.redirect('/');
     }
 };
 
-const deleteBook = async (req, res) => {
+
+const index = async (req, res) => {
     try {
         const user = await User.findById(req.params.userId);
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
+        res.render('books/index.ejs', {
+            user: user,
+            books: user.books || []
+        });
 
-        const ownsBook = user.books.some(
-            (bookId) => bookId.toString() === req.params.bookId
-        );
-
-        if (!ownsBook) {
-            return res.status(404).send('Book not found');
-        }
-
-        const book = await Book.findById(req.params.bookId);
-
-        if (book && book.image && book.image.publicId) {
-            await cloudinary.uploader.destroy(book.image.publicId);
-        }
-
-        await Book.findByIdAndDelete(req.params.bookId);
-
-        user.books.pull(req.params.bookId);
-        await user.save();
-
-        res.redirect(`/users/${user._id}/books`);
     } catch (err) {
         console.log(err);
         res.redirect('/');
@@ -224,5 +148,5 @@ module.exports = {
     delete: deleteBook,
     edit,
     show,
-    update,
-};
+    update
+}
